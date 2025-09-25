f6586ace13fb0efbf35184e27024777418226fbb
Leones Silva de Oliveira <50273318+leonesoliveira@users.noreply.github.com>
2025-09-25 14:56:33 -0300

    update gnreloterecepcao

--- lib/Sped/Gnre/Sefaz/LoteV2.php
+++ lib/Sped/Gnre/Sefaz/LoteV2.php
@@ -49,7 +49,7 @@ class LoteV2 extends Lote {
         $soapEnv->setAttribute('xmlns:soap12', 'http://www.w3.org/2003/05/soap-envelope');
 
         $gnreCabecalhoSoap = $gnre->createElement('gnreCabecMsg');
-        $gnreCabecalhoSoap->setAttribute('xmlns', 'https://www.gnre.pe.gov.br/wsdl/processar');
+        $gnreCabecalhoSoap->setAttribute('xmlns', 'https://www.gnre.pe.gov.br/gnreWS/services/GnreLoteRecepcao');
         $gnreCabecalhoSoap->appendChild($gnre->createElement('versaoDados', '2.00'));
 
         $soapHeader = $gnre->createElement('soap12:Header');
